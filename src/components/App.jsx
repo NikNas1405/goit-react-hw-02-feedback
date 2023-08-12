4f8e807c0d072f8ac29a6bc544b0fdcc95d123e8
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-08-12 15:21:59 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { FeedbackOptions } from './feedback/leave-feedback';
+import { Statistics } from './statistic/statistic';
+import { Section } from './title/title';
+import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countFeedback = option => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+
+    // return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good, neutral, bad } = this.state;
+    let total = good + neutral + bad;
+    return total === 0 ? 0 : (good / total) * 100;
+
+    // let total = this.state.good + this.state.neutral + this.state.bad;
+    // return total === 0 ? 0 : (this.state.good / total) * 100;
+  }
+
+  render() {
+    const options = ['good', 'neutral', 'bad'];
+    return (
+      <div
+        className="statistics"
+        // style={
+        //   {
+        // height: '100vh',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // fontSize: 40,
+        // color: '#010101',
+        // }
+        // }
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            countFeedback={this.countFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
---
+++ src/components/feedback/leave-feedback.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import {
+  FeedbackList,
+  FeedbackListItem,
+  Button,
+} from './leave-feedback.styled';
+
+export const FeedbackOptions = ({ options, countFeedback, title }) => {
+  return (
+    <>
+      {title}
+      <FeedbackList className="feed-btn-list">
+        {options.map(option => (
+          <FeedbackListItem
+            className="feed-btn-item"
+            key={options.indexOf(option)}
+          >
+            <Button onClick={() => countFeedback(option)}>{option}</Button>
+          </FeedbackListItem>
+        ))}
+      </FeedbackList>
+    </>
+  );
+};
